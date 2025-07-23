d13756afa68ea0bebf9ecc3f14c591e8a72141fb
lmsspace <dndejddl1@gmail.com>
2025-07-23 13:40:39 +0900

    스테이지 전환 - 옵저버 패턴

--- Assets/_TestSandBox/LMU/Scripts/Network/Event/NetworkEventSystem.cs
+++ Assets/_TestSandBox/LMU/Scripts/Network/Event/NetworkEventSystem.cs
@@ -10,12 +10,22 @@ public class NetworkEventSystem : BaseManager<NetworkEventSystem>, INetworkRunne
     [Header("이벤트 핸들러")]
     [SerializeField] private PlayerSpawnHandler spawnHandler;
     [SerializeField] private HostDisconnectHandler hostDisconnectHandler;
+
+    // -- 퓨전2 이벤트
     public event Action<NetworkRunner, PlayerRef> OnPlayerJoinedEvent;
     public event Action<NetworkRunner, PlayerRef> OnPlayerLeftEvent;
     public event Action<NetworkRunner, string> OnSceneLoadDoneEvent;
     public event Action<NetworkRunner, string> OnSceneLoadStartEvent;
     public event Action<NetworkRunner, NetDisconnectReason> OnDisconnectedFromServerEvent;
     public event Action<NetworkRunner, ShutdownReason> OnShutdownEvent;
+
+    public event Action<string> OnStageLoadDoneEvent;
+
+    public void TriggerStageLoadDoneEvent(string stageInfo)    // 스테이지 정보 - "3-1 or 5-4"
+    {
+        OnStageLoadDoneEvent?.Invoke(stageInfo);
+    }
+
 
     private void Awake()
     {
@@ -34,6 +44,8 @@ public class NetworkEventSystem : BaseManager<NetworkEventSystem>, INetworkRunne
         OnPlayerLeftEvent -= spawnHandler.OnPlayerLeft;
         OnDisconnectedFromServerEvent -= hostDisconnectHandler.OnDisconnectedFromServer;
         OnShutdownEvent -= hostDisconnectHandler.OnShutdown;
+
+        OnStageLoadDoneEvent = null;
     }
 
     public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
