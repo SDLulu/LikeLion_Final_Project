181aff724d4fb515c3ff4f928a212cde98fb5a99
mingyu8789 <pumkin8789@gmail.com>
2025-07-29 14:56:37 +0900

    RPC 최적화 및 버그 수정
    랜덤타일 클라이언트는 몇개 제대로 스폰이 안되던 버그 수정

--- Assets/_TestSandBox/PMK/Scripts/Tile/PMK_TileZoneSpawner.cs
+++ Assets/_TestSandBox/PMK/Scripts/Tile/PMK_TileZoneSpawner.cs
@@ -1,12 +1,11 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using Fusion;
 
 public class PMK_TileZoneSpawner : MonoBehaviour
 {
-    private Tilemap tilemap;
-    [SerializeField] private GameObject trap;
+    private PMK_TileRogic tileRogic => PMK_TileRogic.Instance;
+    private PMK_TileRPC_Manager tileRPCManager => PMK_TileRPC_Manager.Instance;
+
     [SerializeField] private LayerMask whatisPlatform;
     [SerializeField] private TileBase ruleTile;
 
@@ -15,56 +14,25 @@ public class PMK_TileZoneSpawner : MonoBehaviour
 
     private void Start()
     {
-        tilemap = PMK_TileRogic.Instance.mainTilemap;
         TryPlaceTileIfEmpty();
     }
-
 
     private void TryPlaceTileIfEmpty()
     {
         Vector2 pos = transform.position;
         Collider2D hits = Physics2D.OverlapCircle(pos, 0.01f, whatisPlatform);
 
-        Vector3Int cellPos = tilemap.WorldToCell(pos);
+        Vector3Int cellPos = tileRogic.mainTilemap.WorldToCell(pos);
 
-        //
+        // 해당 셀에 타일이 있는지 확인합니다.
         if (hits == null)
         {
-            // 해당 셀에 타일이 없으면 함정을 배치합니다.
-            if (tilemap.GetTile(cellPos) == null)
+            rnd = Random.Range(0, 100);
+            if (tileRPCManager.HasStateAuthority)
             {
-                int rnd = PMK_TileRogic.Instance.rnd;
-
-                if (rnd > trapSpawnChance)
-                {
-                    // 타일이 없으면 타일을 배치합니다.
-                    tilemap.SetTile(cellPos, ruleTile);
-                    Physics2D.SyncTransforms();
-
-                    PMK_TileRogic.Instance.Create_TileItem(cellPos);
-
-                    Destroy(gameObject);
-                }
-                else
-                {
-
-                    bool isThreeAboveEmpty =
-                    tilemap.GetTile(cellPos + new Vector3Int(0, 1, 0)) == null &&
-                    tilemap.GetTile(cellPos + new Vector3Int(0, 2, 0)) == null &&
-                    tilemap.GetTile(cellPos + new Vector3Int(0, 3, 0)) == null;
-                    Vector3Int downCell = cellPos + Vector3Int.down;
-
-
-
-                    // 아래 셀에 타일이 없고, 위쪽 셀 3개가 비어있을 때 함정 생성
-                    if (isThreeAboveEmpty && tilemap.GetTile(downCell) != null)
-                    {
-                        Vector3 worldPos = tilemap.GetCellCenterWorld(cellPos);
-                        PMK_TileRogic.Instance.RPC_Create_Trap(trap, worldPos);
-                    }
-                    Destroy(gameObject);
-                }
+                tileRPCManager.RPC_DestroyGameObject(rnd, cellPos, trapSpawnChance);
             }
+            Destroy(gameObject);
         }
     }
 }
