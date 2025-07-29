a0aa1021fdb5a2d9c89265384ceee5f8ad54a304
SDLulu <52689979+SDLulu@users.noreply.github.com>
2025-07-29 14:45:02 +0900

    Held태그추가

--- Assets/_TestSandBox/KYW/Scripts/Interaction/ExampleBase/IPlayerInteraction.cs
+++ Assets/_TestSandBox/KYW/Scripts/Interaction/ExampleBase/IPlayerInteraction.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 // 플레이어 상호작용 인터페이스 (넉백, 데미지, 스턴, 무적, 들기)
-public interface IPlayerInteraction : IKnockbackable, IDamageable, IStunnable, IInvincible
+public interface IPlayerInteraction : IKnockbackable, IDamageable, IStunnable, IInvincible, IHoldable
 {
     // 넉백 (스턴과 함께 적용)
     new void ApplyKnockback(Vector2 force, float stunDuration = 0f);
