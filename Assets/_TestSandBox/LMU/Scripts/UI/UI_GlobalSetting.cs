dd671eb7bf80906a0b2afbe4b058c856c7c7cdb1
lmsspace <dndejddl1@gmail.com>
2025-07-30 10:38:05 +0900

    빌드 오류 해결

--- Assets/_TestSandBox/LMU/Scripts/UI/UI_GlobalSetting.cs
+++ Assets/_TestSandBox/LMU/Scripts/UI/UI_GlobalSetting.cs
@@ -39,6 +39,7 @@ public class UI_GlobalSetting : MonoBehaviour
 
     private void LoadSceneInfoList()
     {
+        #if UNITY_EDITOR
         if (_globalSetting.SettingData.IsLoadScenes)
         {
             _sceneDropdown.options.Clear();
@@ -66,10 +67,12 @@ public class UI_GlobalSetting : MonoBehaviour
         {
             _currentFocusSceneText.text = "None";
         }
+        #endif
     }
 
     private void OnSceneListRefreshButtonClick()
     {
+        #if UNITY_EDITOR
         _globalSetting.SettingData.RegisterAllSceneInfo();
         _sceneDropdown.options.Clear();
         foreach (var scene in _globalSetting.SettingData.SceneInfoList)
@@ -80,10 +83,12 @@ public class UI_GlobalSetting : MonoBehaviour
         // FocusScene으로 설정
         int value = _globalSetting.SettingData.SceneInfoList.IndexOf(_globalSetting.SettingData.FocusScene);
         OnSceneDropdownValueChanged(value);
+        #endif
     }
 
     private void OnSceneDropdownValueChanged(int value)
     {
+        #if UNITY_EDITOR
         if(value < 0 || value >= _globalSetting.SettingData.SceneInfoList.Count)
         {
             return;
@@ -91,6 +96,7 @@ public class UI_GlobalSetting : MonoBehaviour
         _globalSetting.SettingData.FocusScene = _globalSetting.SettingData.SceneInfoList[value];
         _currentFocusSceneText.text = $"{_sceneDropdown.options[value].text}";
         _globalSetting.SettingData.IsFocusScene = true;
+        #endif
     }
 
 
