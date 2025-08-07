4eaf52420e3960ae087ebc8433432b1d6eee5dd9
mingyu8789 <pumkin8789@gmail.com>
2025-08-07 18:04:47 +0900

    폭탄 파괴 수정 완료 + 맵 추가 코루틴 추가 + 맵 최적화 수정 + 대,중,소 아이템타일 추가
    폭탄파괴가 정상적으로 파괴시킴 + 전보다 더 정확하게 파괴함
    폭탄 파괴가 함정들도 파괴함
    
    맵 타일 for 루틴에 코루틴으로 타일을 추가하고 아이템 타일들도 추가함
    
    맵 최적화 수정
    - 맵 오브젝트들 이미지 최적화
    - 플레이어 점프가 닿지 않는 구간 수정
    
    아이템 타일 대 중 소 추가 및 등장 확률 15%로 조정

--- Assets/_TestSandBox/PMK/Scripts/Object/PMK_DeathTrap.cs
+++ Assets/_TestSandBox/PMK/Scripts/Object/PMK_DeathTrap.cs
@@ -8,28 +8,26 @@ public class PMK_DeathTrap : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!tileRPC_Manager.HasStateAuthority) return;
         Debug.Log("들어옴");
+        if (!tileRPC_Manager.HasStateAuthority) return;
 
         // 또는 collision.collider.gameObject 사용해도 됨
         if (collision.gameObject.layer != LayerMask.NameToLayer("Player")) return;
 
         NetworkObject netObj = collision.gameObject.GetComponentInParent<NetworkObject>();
         if (netObj == null) return;
-        Debug.Log("실행");
         tileRPC_Manager.RPC_SetPlayerGravity(netObj.InputAuthority, 0.1f);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!tileRPC_Manager.HasStateAuthority) return;
-        Debug.Log("나감");
 
         if (collision.gameObject.layer != LayerMask.NameToLayer("Player")) return;
 
         NetworkObject netObj = collision.gameObject.GetComponentInParent<NetworkObject>();
         if (netObj == null) return;
 
-        tileRPC_Manager.RPC_SetPlayerGravity(netObj.InputAuthority, 0.1f);
+        tileRPC_Manager.RPC_SetPlayerGravity(netObj.InputAuthority, 1.0f);
     }
 }
