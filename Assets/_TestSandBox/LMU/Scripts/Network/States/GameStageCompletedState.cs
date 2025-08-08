c80c7d0bf20842cecb88b624582444e0bc63be8b
mingyu8789 <pumkin8789@gmail.com>
2025-08-08 17:53:57 +0900

    보스 스테이지 전환 완료 (민웅님 스크립트에 추가 된거라 허락받고 정식으로 추가 될 예정)
    현재 맵 로드시 1-1 부터가 아닌
    박민규 스크립트를 사용한 뒤 다음 스테이지를 누르면 1-1로 넘어감
    내 스크립트에서 추가 할려고 해봤으나 적용이 안되어 포기

--- Assets/_TestSandBox/LMU/Scripts/Network/States/GameStageCompletedState.cs
+++ Assets/_TestSandBox/LMU/Scripts/Network/States/GameStageCompletedState.cs
@@ -80,6 +80,7 @@ public class GameStageCompletedState : BaseStateBehaviour
         minWaitingTimer = TickTimer.None;
         _bgTaskTCS?.Clear();
         _bgTaskTCS = null;
+        _stageDataIndex++;
         base.OnExitState();
     }
 
