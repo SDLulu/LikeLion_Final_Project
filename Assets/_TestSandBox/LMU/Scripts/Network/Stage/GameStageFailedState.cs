a4da2733a2a5089f346453669edcb4af55373c70
lmsspace <dndejddl1@gmail.com>
2025-08-17 21:32:29 +0900

    컷신 조정

--- Assets/_TestSandBox/LMU/Scripts/Network/Stage/GameStageFailedState.cs
+++ Assets/_TestSandBox/LMU/Scripts/Network/Stage/GameStageFailedState.cs
@@ -9,7 +9,7 @@ public class GameStageFailedState : BaseStateBehaviour
     public override E_StateName StateName => E_StateName.FailedState;
 
     private TickTimer _fadeOutTimer = TickTimer.None;
-    private const float _fadeOutTime = 2.0f;
+    private const float _fadeOutTime = 3.0f;
     protected override void OnEnterState()
     {
         if (Runner.IsServer)
@@ -20,7 +20,6 @@ public class GameStageFailedState : BaseStateBehaviour
 
             // 로비로 이동시 세션정보 초기화, 다른 유저의 네트워크 접속 허용
             LobbyManager.Inst.UpdateSessionInfo(isInGame: false);
-            StateOwner.DelayForceActiveState<LobbyState>();
         }
     }   
 
